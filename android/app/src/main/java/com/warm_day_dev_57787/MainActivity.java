9f6975ca10d4f0c9e74e953d5579b39d809ab6b1
Crowdbotics <engineering@crowdbotics.com>
2023-01-18 17:39:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/warm_day_dev_57787/MainActivity.java
@@ -0,0 +1,15 @@
+package com.warm_day_dev_57787;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "warm_day_dev_57787";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "warm-day-dev-57787",
+  url: "https://warm-day-dev-57787.botics.co",
+  api: "https://warm-day-dev-57787.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
